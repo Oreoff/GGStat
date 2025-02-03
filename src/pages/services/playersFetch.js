8735ae15d2abf6df7b011945a6f7da15db7b2e9e
Oreoff <maximmelnik37@gmail.com>
2025-02-03 18:52:22 +0200

    Remove live-editing

--- src/pages/services/playersFetch.js
+++ src/pages/services/playersFetch.js
@@ -1,19 +1,6 @@
-export default async function fetchPlayers(updateBeforeFetch = true) {
+export default async function fetchPlayers() {
   try {
-    if (updateBeforeFetch) {
-      const postResponse = await fetch('http://localhost:5120/api/players/save', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json', // Додано Content-Type
-        },
-      });
-
-      if (!postResponse.ok) {
-        console.error(`POST failed with status: ${postResponse.status}`);
-        throw new Error(`HTTP error! status: ${postResponse.status}`);
-      }
-      console.log("Database updated successfully.");
-    }
+    
     const getResponse = await fetch('http://localhost:5120/api/players', {
       method: 'GET',
       headers: {
